1d36402b3fccfbf2a20d9000b48ec7bb82a5da60
zmx4284979 <zmx4284979@163.com>
2024-08-27 16:12:29 +0800

    word check program

--- translation.py
+++ translation.py
@@ -9,7 +9,9 @@ import re
 import sqlite3
 import datetime
 import warnings
- 
+import pandas as pd
+import nltk
+
 # 忽略所有的 DeprecationWarning 警告
 warnings.filterwarnings('ignore', category=DeprecationWarning)
 warnings.filterwarnings('ignore', category=FutureWarning)
@@ -18,6 +20,7 @@ _ = load_dotenv(find_dotenv())    # read local .env file
 
 coze_api_key = os.environ["COZE_API_KEY"]
 COZE_BOT_ID = os.environ["COZE_BOT_ID"]
+COZE_BOT_WORD = os.environ["COZE_BOT_WORD"]
 DB_NAME = os.environ["DB_NAME"]
 
 def call_coze_api(query,con_id='123',user_id='zmx', stream=False):
@@ -153,6 +156,68 @@ def get_bot_direct_reply(api_response):
     answer = bot_answer.get('answer', "")
     return answer, reflection, intermediate, classificationId, initial
 
+
+def send_request_word(personal_access_token, con_id, bot_id, user_id, query, custom_variables):
+    # 填充调用Coze API的具体代码，获得coze的回复，返回为json格式
+    url = 'https://api.coze.cn/open_api/v2/chat'
+    
+    headers = {
+        'Authorization': f'Bearer {personal_access_token}',
+        'Content-Type': 'application/json',
+        'Accept': '*/*',
+        'Host': 'api.coze.cn',
+        'Connection': 'keep-alive'
+    }
+    
+    data = {
+        'conversation_id': con_id,
+        'bot_id': bot_id,
+        'user': user_id,
+        'query': query,
+        'stream': False,
+        'custom_variables':{
+            'suggestions': custom_variables
+        }
+    }
+    
+    response = requests.post(url, headers=headers, json=data)
+    
+    return response.json()
+
+def get_word_suggestions(input_text, list_of_table):
+    # Convert the list of lists into a pandas DataFrame
+    df = pd.DataFrame(list_of_table[1:], columns=list_of_table[0])
+    # 将df第一列中的所有字符串都转换成小写
+    df.iloc[:, 0] = df.iloc[:, 0].str.lower()
+    # 将df中所有元素进行字符串清洗，去除多余空格，换行符，制表符等
+    df = df.applymap(lambda x: x.strip() if isinstance(x, str) else x)
+    # 将input_text中的所有单词都转换成小写
+    input_text = input_text.lower()
+    # 使用nltk.word_tokenize将input_text中的单词进行分词
+    token_text = nltk.word_tokenize(input_text)
+    # 使用nltk.pos_tag将input_text中的单词进行词性标注
+    token_text = nltk.pos_tag(token_text,tagset='universal')
+    check_list = []
+    # 将token_text中的每一个元素在df的Non-STE（对应元素中的第一个值）和N-POS（对应元素中的第二个值）中匹配查找，如果找到了，则将df对应行添加到check_list中
+    for word in token_text:
+        # 查找df中是否有与word[0]相同的值，并返回行号
+        row_index = df[df['Non-STE'] == word[0]].index
+        # 如果找到了，则将df对应行添加到check_list中
+        if not row_index.empty:
+            # 检查df中对应行的N-POS是否为空，或者是否与word[1]相同
+            #if df.loc[row_index[0]][1] == "" or df.loc[row_index[0]][1] == word[1]:
+            #    check_list.append(df.loc[row_index[0]].tolist())
+            check_list.append(df.loc[row_index[0]].tolist())
+    # 将check_list中元素去重
+    check_list = list(set(tuple(row) for row in check_list))
+    # 将check_list中每个元素生成修改建议字符串，返回list
+    word_suggestions = []
+    for i in range(len(check_list)):
+        suggest = "replace word \'" + check_list[i][0] + "\' with \'" + check_list[i][2] + "\'"
+        suggest = suggest.lower()
+        word_suggestions.append(suggest)
+    return word_suggestions
+
 def clear_messages():
     st.session_state.messages = []
 
@@ -162,7 +227,7 @@ def testcall():
     time.sleep(1)
     return api_response
 
-def db_record(query, answer, reflection,intermediate, classificationId, initial):
+def db_record(query, answer, reflection,intermediate, classificationId, initial, result, custom_variables):
     # 将query和answer记录到数据库
     # 连接到数据库，如果数据库不存在，则会创建一个新的数据库
     conn = sqlite3.connect(DB_NAME)
@@ -170,12 +235,12 @@ def db_record(query, answer, reflection,intermediate, classificationId, initial)
     cursor = conn.cursor()
     # 创建表
     cursor.execute('''CREATE TABLE IF NOT EXISTS datatable
-                (date, query, answer, intermediate, reflection, classificationId, initial)''')
+                (date, query, answer, intermediate, reflection, classificationId, initial, result, custom_variables)''')
     # 插入数据
     # 这里需要将传入的参数转换为元组(tuple)，然后传递给execute方法
-    cursor.execute("INSERT INTO datatable VALUES (?, ?, ?, ?, ?, ?, ?)",
+    cursor.execute("INSERT INTO datatable VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
                    (datetime.datetime.now(), query, answer, intermediate, 
-                    reflection, classificationId, initial))
+                    reflection, classificationId, initial, result, custom_variables))
     # 提交事务
     conn.commit()
     # 关闭连接
@@ -202,6 +267,8 @@ def main():
         st.session_state.messages = []  
     # 历史对话展示框
     #messages = st.container(height=450)
+    # 字词字典
+    list_of_table=[["Non-STE","N-POS","STE","POS"],["acceptable ","","PERMITTED ",""],["alternate ","","ALTERNATIVE ",""],["any ","","None",""],["avoid ","VERB","PREVENT ","VERB"],["both ","","THE TWO ","NOUN"],["check ","VERB","CHECK ","NOUN"],["cover ","VERB","COVER ","NOUN"],["damage ","VERB","DAMAGE ","NOUN"],["ensure ","VERB","MAKE SURE ","VERB"],["fit ","VERB","INSTALL ","VERB"],["follow ","VERB","OBEY ","VERB"],["further ","","MORE ",""],["further ","","MORE ",""],["have to ","VERB","MUST ","VERB"],["insert ","VERB","PUT ","VERB"],["main ","","PRIMARY ",""],["may ","","CAN ","VERB"],["need ","VERB","NECESSARY ",""],["now ","","AT THIS TIME",""],["over ","PRT","ABOVE,ON,ALONG ","PRT"],["perform ","VERB","DO ","VERB"],["press ","VERB","PUSH ","VERB"],["reach ","VERB","GET ","VERB"],["repeat ","VERB","DO,AGAIN",""],["required ","VERB","NECESSARY ",""],["rotate ","VERB","TURN ","VERB"],["secure ","VERB","ATTACH ","VERB"],["shall ","","MUST ","VERB"],["should ","","MUST ","VERB"],["since ","","BECAUSE ",""],["test ","VERB","TEST ","NOUN"],["therefore ","","THUS,AS A RESULT",""]]
     # 显示整个对话历史
     for message in st.session_state.messages:
         st.chat_message(message["role"]).write(message["content"], unsafe_allow_html=True)
@@ -248,6 +315,7 @@ def main():
             # 二次修改
             if answer is None:
                 answer = ""
+            rule_answer = answer
             answer = "二次修改："+answer
             # 将LLM的回答添加到对话历史中
             st.session_state.messages.append({"role": "assistant", "content": answer})
@@ -260,11 +328,28 @@ def main():
                 st.chat_message("assistant").write(reflection, unsafe_allow_html=True)
             else:
                 reflection = ""
+            # 字词修改
+            custom_variables=get_word_suggestions(rule_answer,list_of_table)
+            custom_variables = str(custom_variables)
+            if custom_variables != []:
+                result = send_request_word(coze_api_key,'123', COZE_BOT_WORD, 'zmx', rule_answer, custom_variables)
+                result = parse_message_object(result)
+                result = result.get('answer', "")
+                custom_variables="字词修改意见："+custom_variables
+                st.session_state.messages.append({"role": "assistant", "content": custom_variables})
+                st.chat_message("assistant").write(custom_variables, unsafe_allow_html=True)
+            else:
+                custom_variables = ""
+                result = rule_answer 
+            result = "最终结果："+result
+            st.session_state.messages.append({"role": "assistant", "content": result})
+            st.chat_message("assistant").write(result, unsafe_allow_html=True)
             # 调用函数，记录对话历史到数据库
-            db_record(prompt, answer, reflection,intermediate, classificationId, initial)
+            db_record(prompt, answer, reflection,intermediate, classificationId, initial, result, str(custom_variables))
 
 
 if __name__ == '__main__':
+    # 环境 conda active llm-universe
     # 命令行启动 streamlit run translation.py
     # 持续运行 nohup streamlit run translation.py --server.port 8504 >translation.log &
     # 监控输出 tail -f translation.log
